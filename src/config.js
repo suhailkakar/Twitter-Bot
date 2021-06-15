12c7a8d31ad636c60532d2641c433e07333d0632
Suhail Kakar <suhail.zahed@gmail.com>
2021-06-15 12:21:07 +0530

    Updated Config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     consumer_key: 'YOUR_CONSUMER_KEY',
     consumer_secret: 'YOUR_CONSUMER_SECRET_KEY',
-    access_token: 'YOUR_ACCESS_TOKE',
+    access_token: 'YOUR_ACCESS_TOKEN',
     access_token_secret:  'YOUR_TOKEN_SECRET'
   }
