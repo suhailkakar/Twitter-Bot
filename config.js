37cc81988056ac5bc18eccc81800f8e22757bbb7
Suhail Kakar <suhail.zahed@gmail.com>
2021-06-15 11:06:40 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:'YOUR_CONSUMER_KEY',
+    consumer_secret: 'YOUR_CONSUMER_SECRET',
+    access_token:         'YOUR_ACCESS_TOKE',
+    access_token_secret:  'YOUR_TOKEN_SECRET'
+  }
