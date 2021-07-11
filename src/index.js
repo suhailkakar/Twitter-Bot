c21b3e2b605b2881c1b568f8b6431e7ad04e36c6
Suhail Kakar <56798748+suhailkakar@users.noreply.github.com>
2021-07-11 21:52:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,65 @@
-const Twit = require("twit");
-const twit = new Twit(require("./config.js"));
-const mediaArtsSearch = { q: "#javascript", count: 100, result_type: "recent" };
-const retweetLatest = () => {
-  twit.get("search/tweets", mediaArtsSearch, (error, data) => {
-    if (error) {
-      console.log(error.message);
-    } else {
-      const retweetId = data.statuses[0].id_str;
-      twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
-        if (response) {
-          console.log("Success! Kakar's bot has retweeted something.");
-        }
-        if (error) {
-          console.log(error.message);
-        }
-      });
-    }
-  });
-};
+const config = require('./config')
+const twit =  require('twit')
 
-retweetLatest();
-setInterval(retweetLatest, 1000 * 60 * 30);
+const T = new twit(config)
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+function retweet(searchText) {
+    let params = {
+        q : searchText + '',
+        result_type : 'mixed',
+        count : 25,
+    }
+
+    T.get('search/tweets', params, function(err_search, data_search, response_search){
+
+        let tweets = data_search.statuses
+        if (!err_search)
+        {
+            let tweetIDList = []
+            for(let tweet of tweets) {
+
+                if(tweet.text.startsWith("RT @")){
+               
+                    console.log("\nStarts with RT@, adding retweeted status id_str")
+                    if (tweet.retweeted_status) {
+                        tweetIDList.push(tweet.retweeted_status.id_str);
+                    }
+                    else {
+                        tweetIDList.push(tweet.id_str);
+                    }
+                }
+                else {
+                    tweetIDList.push(tweet.id_str);
+                }
+            }
+
+            tweetIDList = tweetIDList.filter( onlyUnique )
+
+            console.log("TweetID LIST = \n" + tweetIDList)
+
+            for (let tweetID of tweetIDList) {
+                T.post('statuses/retweet/:id', {id : tweetID}, function(err_rt, data_rt, response_rt){
+                    if(!err_rt){
+                        console.log("\n\nRetweeted! ID - " + tweetID)
+                    }
+                    else {
+                        console.log("\nError... Duplication maybe... " + tweetID)
+                        console.log("Error = " + err_rt)
+                    }
+
+                })
+            }
+        }
+        else {
+            console.log("Error while searching" + err_search)
+            process.exit(1)
+        }
+    })
+}
+
+// Run every 60 seconds
+setInterval(function() { retweet('#javascript OR #typescript'); }, 30000)
